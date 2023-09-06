3d2a971dea6362d0cd4f1acb08a071a9a1b67059
Guilherme Reis <guilhermereisdl@gmail.com>
2023-09-06 11:24:18 -0300

    Project created

---
+++ src/main/java/com/example/webservicejpaspring/WebserviceJpaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.webservicejpaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceJpaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceJpaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webservicejpaspring/WebserviceJpaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservicejpaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceJpaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
