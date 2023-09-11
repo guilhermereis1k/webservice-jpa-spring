41db3caaa43a564eaa99c930c420cbdd29ec64cf
Guilherme Reis <guilhermereisdl@gmail.com>
2023-09-11 13:14:42 -0300

    Products/Categories added, Order entitie added, many to many and one to many associations made.

---
+++ src/main/java/com/example/webservicejpaspring/repositories/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.example.webservicejpaspring.repositories;
+
+import com.example.webservicejpaspring.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
